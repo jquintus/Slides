6a3b1f96d53dd6d250eaabdce9beb5dba7d9d06d
Josh Quintus <josh@dotalign.com>
2017-11-10 13:06:07 -0500

    Typo

--- src/Records/ConflictingTypes.fs
+++ src/Records/ConflictingTypes.fs
@@ -4,5 +4,5 @@ type Magazine = { title: string; pubDate: DateTime }
 
 let mad = { title = "Mad"; pubDate = new DateTime(1952, 10, 1)}
 
-let hitchiker = { Book.title = "Hithiker's Guide to the Galaxy"
+let hitchiker = { Book.title = "The Hitchhiker's Guide to the Galaxy"
                   pubDate = new DateTime(1979, 10, 12) }
