bef2af5e752bcb52fe8c7e640161828745104136
Josh Quintus <josh@dotalign.com>
2017-11-10 12:59:41 -0500

    Formatting

--- src/Records/With.fs
+++ src/Records/With.fs
@@ -4,4 +4,4 @@ let marty = { firstName = "Marty"; lastName = "McFly" }
 // val marty : Person = {firstName = "Marty"; lastName = "McFly";}
 
 let george =  { marty with firstName = "George"}
-// val george : Person = {firstName = "George"; lastName = "McFly";}
+// val george : Person = {firstName = "George";lastName = "McFly";}
